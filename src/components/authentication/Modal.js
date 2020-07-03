2b129459dcd92f0fa1dc319593b6b1717b0cb5a7
Aniket Kumbhar <akumbhar@ch.iitr.ac.in>
2020-07-03 13:41:41 +0530

    added popup

--- src/components/authentication/Modal.js
+++ src/components/authentication/Modal.js
@@ -1,5 +1,6 @@
 import React,{useState} from 'react'
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter,  } from 'reactstrap';
+import {Image} from 'react-bootstrap'
 import {Link} from 'react-router-dom';
 function ModalComponent(props) {
 
@@ -15,10 +16,15 @@ function ModalComponent(props) {
         <ModalHeader toggle={toggle}>Login/Register for more updates</ModalHeader>
         <ModalBody>
             Want to get updates on the newest news available? Then, click on the following links for register/login.
+           <br/>
+           <br/>
+           If you have an account, then click on login otherwise click on register.
+
         </ModalBody>
         <ModalFooter>
+
           <Link to = "/register"><Button color="primary" onClick={toggle}>Register</Button></Link>
-          <Link to = "/login"><Button color="secondary" onClick={toggle}>Login</Button></Link>
+          <Link to = "/login"><Button color="danger" onClick={toggle}>Login</Button></Link>
         </ModalFooter>
       </Modal>
     </div>
